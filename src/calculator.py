733dae508d191d19694c02831aa86a995dd0e20e
Gorka Moreno Asín <gorka@540deg.com>
2023-10-26 13:00:13 +0200

    Change assertpy to hamcrest

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,6 @@
-
 def add(number1: int, number2: int) -> int:
-    return number1 + number2
+    return 0
+
 
 def multiply(number1: int, number2: int) -> int:
-    return number1 * number2
+    return 0
